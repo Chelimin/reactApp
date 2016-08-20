51a192b49ff3beea37887abcfc661b997c9fbf4e
Catherine He <chelimin@gmail.com>
2016-08-19 17:10:09 -0700

    add redux todo list

--- App.jsx
+++ App.jsx
@@ -1,43 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { addTodo } from './actions/actions'
 
-class App extends React.Component {
+import AddTodo from './components/AddTodo.js'
+import TodoList from './components/TodoList.js'
+
+class App extends Component {
    render() {
+      const { dispatch, visibleTodos } = this.props
+
       return (
          <div>
-            <h3>Array: {this.props.propArray}</h3>
-            <h3>Bool: {this.props.propBool ? "True..." : "False..."}</h3>
-            <h3>Func: {this.props.propFunc(3)}</h3>
-            <h3>Number: {this.props.propNumber}</h3>
-            <h3>String: {this.props.propString}</h3>
-            <h3>Object: {this.props.propObject.objectName1}</h3>
-            <h3>Object: {this.props.propObject.objectName2}</h3>
-            <h3>Object: {this.props.propObject.objectName3}</h3>
+
+            <AddTodo
+               onAddClick = {text =>
+               dispatch(addTodo(text))}
+            />
+
+            <TodoList todos = {visibleTodos}/>
+
          </div>
-      );
+      )
    }
 }
 
-App.propTypes = {
-   propArray: React.PropTypes.array.isRequired,
-   propBool: React.PropTypes.bool.isRequired,
-   propFunc: React.PropTypes.func,
-   propNumber: React.PropTypes.number,
-   propString: React.PropTypes.string,
-   propObject: React.PropTypes.object
-}
-
-App.defaultProps = {
-   propArray: [1,2,3,4,5],
-   propBool: true,
-   propFunc: function(e){return e},
-   propNumber: 1,
-   propString: "String value...",
-
-   propObject: {
-      objectName1:"objectValue1",
-      objectName2: "objectValue2",
-      objectName3: "objectValue3"
+function select(state) {
+   return {
+      visibleTodos: state.todos
    }
 }
 
-export default App;
+export default connect(select)(App)
